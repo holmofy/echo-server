f5d61016137bb036c89612f6441a92f311f63ef9
胡飞飞 <hu.feifei@darcytech.com>
2019-04-14 20:00:58 +0800

    adjust include

--- tcp-echo-server-multithread.c
+++ tcp-echo-server-multithread.c
@@ -1,10 +1,8 @@
+#include <stdio.h>
 #include <stdlib.h>
-#include <errno.h>
-#include <unistd.h>
 #include <string.h>
-#include <sys/wait.h>
+#include <unistd.h>
 #include <sys/socket.h>
-#include <resolv.h>
 #include <arpa/inet.h>
 #include <pthread.h>
 
--- tcp-echo-server.c
+++ tcp-echo-server.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
 #include <unistd.h>
+#include <sys/socket.h>
 #include <arpa/inet.h>
 
 #define BUFFER_SIZE 1024
